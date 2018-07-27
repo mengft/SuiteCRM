a63f24f04cd837f9f5dce6e779eb36cb942a0a63
mengft <fantao.meng@healscitech.com>
2018-07-27 10:31:45 +0800

    项目安装

---
+++ cache/smarty/templates_c/%%75^75C^75CFA820%%recaptcha_disabled.tpl.php
@@ -0,0 +1,21 @@
+<?php /* Smarty version 2.6.31, created on 2018-07-27 02:31:26
+         compiled from /Users/mengft/Project/H5/SuiteCRM/include/utils/recaptcha_disabled.tpl */ ?>
+<?php echo '
+<script>
+
+  /**
+   * Login Screen Validation
+   */
+  function validateAndSubmit() {
+      generatepwd();
+    }
+
+  /**
+   * Password reset screen validation
+   */
+  function validateCaptchaAndSubmit() {
+      document.getElementById(\'username_password\').value = document.getElementById(\'new_password\').value;
+      document.getElementById(\'ChangePasswordForm\').submit();
+    }
+</script>
+'; ?>
